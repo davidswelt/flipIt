e7480cbb0c9f065fac4daf1f3699ee3ffdfef633
Alan Nochenson <anochenson@psu.edu>
2013-02-15 06:18:43 +0000

    Lowered max runs to 12

--- config/config.sample.inc.php
+++ config/config.sample.inc.php
@@ -1,5 +1,8 @@
 <?php
 
-define('MAX_RUNS_PER_SESSION', 10);
+define('MAX_RUNS_PER_SESSION', 12);
+define('NUM_PRACTICE_RUNS', 2);
+define('BONUS_MULTIPLIER', 5); //actually 50c
+ 
 define('DB_USERNAME','sample');
 define('DB_PASSWORD','pass');
