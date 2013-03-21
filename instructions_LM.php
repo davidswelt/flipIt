b40696b7d5466c10aee84e812c6e00a5b4e08781
Alan Nochenson <anochenson@psu.edu>
2013-03-21 22:42:07 +0000

    new david instructions are in

--- instructions_LM.php
+++ instructions_LM.php
@@ -1,35 +1,50 @@
 <div id="rules_panel">
  <h2>Basic Rules</h2>
+<ul style='list-style-type: none'><li>
 <p>You will be playing multiple rounds of a two-player game called
  FlipIt. The objective of FlipIt is to gain and maintain possession of
  the game board. The board has two colors - red one one side, and blue
  on the other, and it can be flipped by either you or the other
  player.  When it shows your color ("blue"), you have possession of
- the board. </p>
+ the board.</p> 
 
 <p>However, until you flip the board, the state of possession of the game board is hidden from your view. In this state, you will not see the board on the screen.</p> 
+
 <p>The only action you have available is to 'flip' the game board. When you "flip" the board, you will see the board, but the game will also reveal some important information.</p>
 
-<p>If you flip at a time when you already own the board, then you will see a large blue "X" on the screen.  You will maintain possession of the board, and it won't actually change colors.</p>
+<p>If you flip and regain possession of the board from the opponent, you
+  will see a graphic on the screen that shows how long you had the
+  board beforehand (a blue bar), and how long the opponent had it
+  until you flipped (a red bar).  You will also see whether the
+  opponent flipped multiple times (more than necessary): one or more
+  small red "X" symbols will indicate this.  If you flip at a time when you already own the board, then you will see a large "X" on the screen.  You will maintain possession of the board, and it won't actually change colors.</p>
+<p>All of this information will only be shown to you and not your opponent.</p>
+ 
+  <p>Let's take a look at a few examples
+	  from a game.</p>
+	  <center><img src='images/midgame3.png' />   </center>
+<p>You can see that you had the board for a while (blue bar) until the opponent
+	  flipped, and that you then waited a long time before
+	  flipping back.  The opponent flipped two more times without
+	  need, as indicated by the two red 'X'.  So, you should have
+	  flipped much earlier.</p>
+<p>Consider the next chart:</p>
+<center><img src='images/midgame4.png' /></center>
 
-<p>If you flip and regain possession of the board from the opponent, you will see a graphic on the screen that shows how long you had the board beforehand (a blue bar), and how long the opponent had it until you flipped (a red bar).  You will also see whether the opponent flipped multiple times (more than necessary): one or more small red "X" symbols will indicate this.</p>
+<p>Here, you flipped earlier, though not early enough.  The opponent
+flipped soon after your flip, and then one more time.</p>
+<p>The next situation shows what happens when you flip while already
+	  owning the board.  This extra flip will cost you 100
+	  points:</p>
 
-<p>At the time of your flip, this information will only be shown to you and not to your opponent.</p>
-  
+	<center>  <img src='images/blue_x.png' width='200' /> </center>
+
+<p>Again, you cannot observe if and when the opponent flipped the board
+in the past, until you take the action to flip the board yourself.</p>
+
 <p>The same rules apply to your opponent.</p>
 
-<p>Again, you cannot observe if and when the opponent flipped the board in the past, until you take the action to flip the board yourself.</p>
-<BR>
-  
-  
-	  <h2 style=''>An example game:</h2>
-	  <h4>After a flip was made by you:</h4>
-	  <img src='images/midgame2.png'/>
-	  <h4>A flip made by you when you already own the board:</h4>
-	  <img src='images/blue_x.png' style='margin-left:100px'/>
-
-                                                                   <br>
-Below, we break down the rules in more detail.
+<p>Below, we break down the rules in more detail.</p>
 </li></ul>
 
  <h2>Detailed Rules</h2>
@@ -65,13 +80,15 @@ Below, we break down the rules in more detail.
 	  The board displays the latest available information about the game, including the opponent and your own last possession times leading up to your last flip, or a blue "X" if you flipped while in possession of the board.
 	 
 	  You can only see information that was revealed by your flips.
-
-<p>	The following graphic demonstrates how the game works.  You will never see all of this information at once, but it is helpful to illustrate how things work.</p>
+<br>
+	The following graphic demonstrates how the game works.  <p>You
+	will never see all of this information at once, but it is
+	helpful to illustrate how the game works.</p>
 	
 		  <img src='images/example_game.png' width='800' />
 
 	 <p>   
-	  Let's examine the moves made in the game given above.
+	  Let's examine the moves made in an example game:
 	  </p>
 	  <ul>
 		 <li>
