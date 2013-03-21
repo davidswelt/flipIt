00b23590394be468686b2448782367864a36dbfa
Alan Nochenson <anochenson@psu.edu>
2013-03-21 01:52:23 +0000

    Swapped instructions for hardcoded LM instructions

---
+++ instructions_LM.php
@@ -0,0 +1,128 @@
+<div id="rules_panel">
+ <h2>Basic Rules</h2>
+<p>You will be playing multiple rounds of a two-player game called
+ FlipIt. The objective of FlipIt is to gain and maintain possession of
+ the game board. The board has two colors - red one one side, and blue
+ on the other, and it can be flipped by either you or the other
+ player.  When it shows your color ("blue"), you have possession of
+ the board. </p>
+
+<p>However, until you flip the board, the state of possession of the game board is hidden from your view. In this state, you will not see the board on the screen.</p> 
+<p>The only action you have available is to 'flip' the game board. When you "flip" the board, you will see the board, but the game will also reveal some important information.</p>
+
+<p>If you flip at a time when you already own the board, then you will see a large blue "X" on the screen.  You will maintain possession of the board, and it won't actually change colors.</p>
+
+<p>If you flip and regain possession of the board from the opponent, you will see a graphic on the screen that shows how long you had the board beforehand (a blue bar), and how long the opponent had it until you flipped (a red bar).  You will also see whether the opponent flipped multiple times (more than necessary): one or more small red "X" symbols will indicate this.</p>
+
+<p>At the time of your flip, this information will only be shown to you and not to your opponent.</p>
+  
+<p>The same rules apply to your opponent.</p>
+
+<p>Again, you cannot observe if and when the opponent flipped the board in the past, until you take the action to flip the board yourself.</p>
+<BR>
+  
+  
+	  <h2 style=''>An example game:</h2>
+	  <h4>After a flip was made by you:</h4>
+	  <img src='images/midgame2.png'/>
+	  <h4>A flip made by you when you already own the board:</h4>
+	  <img src='images/blue_x.png' style='margin-left:100px'/>
+
+                                                                   <br>
+Below, we break down the rules in more detail.
+</li></ul>
+
+ <h2>Detailed Rules</h2>
+ <ul>
+	<li>
+	  <h3>Points</h3>
+	  <p>
+	  You gain <b>100</b> points per second that you are in control.
+	  <br>
+	  You earn <b>0</b> points while your opponent is in control.
+	  <br>
+	  You pay <b>100</b> points when you play 'flip'.
+	  <br><br>
+	  You begin with <b>0</b> points. Scores are updated when you play a 'flip' and at the end of the game.
+	  <br>
+	  <br>
+	  </p>
+	</li>
+	
+	<li>  
+	  <h3>Moves</h3>
+	  <p>
+	  Your only move is to play 'flip'.
+	  If you are in control and you play 'flip' you remain in control.
+	  If you are not in control and you play 'flip' you regain control.
+	  Only one player can be in control at a time.
+	  </p>
+	</li>
+ 
+	<li>
+	  <h3>The Board</h3>
+	  <p>
+	  The board displays the latest available information about the game, including the opponent and your own last possession times leading up to your last flip, or a blue "X" if you flipped while in possession of the board.
+	 
+	  You can only see information that was revealed by your flips.
+
+<p>	The following graphic demonstrates how the game works.  You will never see all of this information at once, but it is helpful to illustrate how things work.</p>
+	
+		  <img src='images/example_game.png' width='800' />
+
+	 <p>   
+	  Let's examine the moves made in the game given above.
+	  </p>
+	  <ul>
+		 <li>
+			<i>1st second</i>: The blue player starts in control.
+		 </li>
+		 <li>
+			<i>2nd second:</i> The red player plays 'flip' and gains control. The red player plays 'flip' again less than a second later and remains in control.
+		 </li>
+		 <li> 
+			<i>3rd second:</i> The blue player plays 'flip' and regains control. He maintains control for a bit over 2 seconds.
+		 </li>
+		 <li>
+			<i>5th second:</i> The red player plays 'flip' and regains control. He keeps control for less than a second.
+		 </li>
+		 <li>
+			<i>6th second:</i> The blue player plays 'flip' and regains control. He keeps control for about 2 seconds.
+		 </li>                                                         
+		 <li>
+			<i>7th second:</i> The red player plays 'flip' and regains control. He keeps control for about 1 second.
+		 </li>                                                         
+		 <li>
+			<i>9th second:</i> The blue player plays 'flip' and regains control. He maintains control for 4 seconds.
+		 </li>                                                          
+		 <li>
+			<i>12th second:</i> The red player plays 'flip' and regains control. He maintains control for the rest of the game. He makes a number of flips in which he maintains control.
+		 </li>                                                          
+		 <li>
+			<i>20th second:</i> The game ends.
+		 </li>
+	  </ul>
+	
+	  </p>
+
+	  <p> 
+	  The blue player was in control for <b>8.05</b> seconds earning <b>805</b> points, and played 'flip' 3 times, costing <b>300</b> points. This gives him a total score of <b>505</b> points.
+	  </p>
+	  <p>
+	  The red player was in control for <b>11.95</b> seconds earning <b>1195</b> points, and played 'flip' 9 times, costing <b>900</b> points. This gives him a total score of <b>295</b> points.
+	  </p>
+	  <p>
+	  The blue player has more points than the red player and thus wins.
+	  </p>
+	</li>
+
+	<li>
+	  <h3>Payment</h3> 
+	  <p>
+You will be compensated according to your performance in this study. For completing the study, you are guaranteed the amount listed on the Mechanical Turk HIT that you have accepted, and you will be paid an additional sum based on your performance.<br> 
+You will participate in multiple rounds of the game. At first, you will participate in a practice round without a bonus payment to familiarize yourself with the interface. Then, you will participate in several additional rounds. You can receive a bonus payment for your performance in each of those rounds.<br>
+You can increase your bonus payment in a given round by performing well compared to the red player. If you lose by more than 1000 points, however, you will receive no bonus payment for that round.<br> 
+The exchange rate for points into the bonus payment is 1 cent for 100 points. For example, you would earn a bonus payment of 10 cents by gaining exactly as many points as the red player. If you outperform your opponent by 500 points you would earn 15 cents. If you underperform you opponent by 500 points you would earn 5 cents.<br> 
+	  </p>
+ </ul>
+</div> 
