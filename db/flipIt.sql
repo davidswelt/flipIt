883f875b8a8ad6a2d7c9c1d7efdd336d3a2c1496
Alan Nochenson <anochenson@psu.edu>
2013-10-17 19:05:54 +0000

    Adding in db creation files

---
+++ db/flipIt.sql
@@ -0,0 +1,148 @@
+-- MySQL dump 10.13  Distrib 5.5.33, for Linux (i686)
+--
+-- Host: localhost    Database: flipIt
+-- ------------------------------------------------------
+-- Server version	5.5.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bonus`
+--
+
+DROP TABLE IF EXISTS `bonus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bonus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hit_id` text NOT NULL,
+  `mturk_id` text NOT NULL,
+  `amount` text NOT NULL,
+  `session_id` int(11) NOT NULL,
+  `info_treatment_id` int(11) NOT NULL,
+  `visual_treatment_id` int(11) NOT NULL,
+  `finished` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `paid` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `info_treatment_id` (`info_treatment_id`),
+  KEY `session_id` (`session_id`),
+  CONSTRAINT `bonus_ibfk_4` FOREIGN KEY (`info_treatment_id`) REFERENCES `info_treatment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `bonus_ibfk_5` FOREIGN KEY (`session_id`) REFERENCES `gameSession` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1117 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gameResult`
+--
+
+DROP TABLE IF EXISTS `gameResult`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gameResult` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `run_id` int(11) NOT NULL,
+  `flips` text NOT NULL,
+  `blue_score` text NOT NULL,
+  `red_score` text NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `run_id` (`run_id`),
+  CONSTRAINT `gameResult_ibfk_2` FOREIGN KEY (`run_id`) REFERENCES `gameRun` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3718 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gameRun`
+--
+
+DROP TABLE IF EXISTS `gameRun`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gameRun` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_id` int(11) NOT NULL,
+  `started` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tick` int(11) NOT NULL,
+  `anchor` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `start_time` (`started`),
+  KEY `session_id` (`session_id`),
+  CONSTRAINT `gameRun_ibfk_2` FOREIGN KEY (`session_id`) REFERENCES `gameSession` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4529 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gameSession`
+--
+
+DROP TABLE IF EXISTS `gameSession`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gameSession` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mturk_id` text NOT NULL,
+  `started` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `survey_blob` text NOT NULL,
+  `info_treatment_id` int(11) NOT NULL,
+  `visual_treatment_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `info_treatment_id` (`info_treatment_id`),
+  KEY `visual_treatment_id` (`visual_treatment_id`),
+  CONSTRAINT `gameSession_ibfk_2` FOREIGN KEY (`info_treatment_id`) REFERENCES `info_treatment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `gameSession_ibfk_4` FOREIGN KEY (`visual_treatment_id`) REFERENCES `visual_treatment` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=911 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `info_treatment`
+--
+
+DROP TABLE IF EXISTS `info_treatment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `info_treatment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `information_sets` text NOT NULL,
+  `opponent_description` text NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `visual_treatment`
+--
+
+DROP TABLE IF EXISTS `visual_treatment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `visual_treatment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `feedback_type` text NOT NULL,
+  `feedback_type_full` text NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-10-17 19:04:18
