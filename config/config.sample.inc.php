2722f93b61d3d4b15b0421e1afcbf804cf0902b5
Alan Nochenson <anochenson@psu.edu>
2013-10-17 18:53:48 +0000

    edits to config sample for better demo mode

--- config/config.sample.inc.php
+++ config/config.sample.inc.php
@@ -1,12 +1,26 @@
 <?php
 
-define('MAX_RUNS_PER_SESSION', 5);
-define('NUM_PRACTICE_RUNS', 1);
+//Uncomment to turn on demo mode
+//define('DEMO','demo');             
+
+if(defined('DEMO')) {
+	define('MAX_RUNS_PER_SESSION', 600);
+	define('NUM_PRACTICE_RUNS',-1);
+
+}
+else { //not demo
+	define('MAX_RUNS_PER_SESSION', 6);
+	define('NUM_PRACTICE_RUNS', 1);
+}
+    
 define('COOKIE_NUM', 1);
 
 //Monetary value of 1 point
 //Currently set at 1/100 of a cent, or 100 points = 1 cent
-define('POINTS_EXCHANGE_RATE', '0.0001');
-    
+define('POINTS_EXCHANGE_RATE', '0.0001');  
+
 define('DB_USERNAME','sample');
 define('DB_PASSWORD','pass');
+
+
+
